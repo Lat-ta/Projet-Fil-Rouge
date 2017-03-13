ce54aff019ae2c223e0bbd82c9396800ed37042e
Lat-ta <laetitia.carvalho@gmail.com>
2017-03-13 15:08:01 +0100

    Ajout Header Gestionnaire + MàJ CSS
    A améliorer

---
+++ header_gestion.php
@@ -0,0 +1,11 @@
+<div class="header" id="top">
+	
+	<div class="contenu">
+	<img id="logovelov" src="img/logo.jpg" title="Vélo'v">
+		<div id="centre">
+		<h1> Espace Gestionnaire</h1>
+		</div>
+	<img id="logogdlyon" src="img/logo2.jpg">
+	</div>
+	
+</div>
