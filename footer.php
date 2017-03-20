7eec50c9179b26ba9b8c40d5215f36a09d885898
Lat-ta <laetitia.carvalho@gmail.com>
2017-03-20 10:38:49 +0100

    Footer

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
 			
 	<ul id="footer-menu">
 		<li>
+		<b><a href="mentions_legales.php" style="color:#FFFFFF" >Mentions légales</a></b>
 		</li>
 		<li>
 		<b><a href="#top" style="color:#FFFFFF">Haut de page</a></b>
