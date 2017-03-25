ab53abe07ac2e9225b308a9eb25b6c177323a307
Lat-ta <laetitia.carvalho@gmail.com>
2017-03-25 14:37:39 +0100

    ok

--- Projet-Fil-Rouge/View/connexion.php
+++ Projet-Fil-Rouge/View/connexion.php
@@ -1,36 +1,40 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="ISO-8859-1">
-<title>Insert title here</title>
-<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-</head>
-<body id="bdy">
-	<div id="bloc_page">
-
-
-
-	<div id="banni�re">
-			<div id="banni�re_description">
-			<b>Bienvenue sur le site troc de temps! </b>
-			</div>		
-	    </div>
+	<head>
+	<meta charset="ISO-8859-1">
+	<link rel="stylesheet" href="style.css"/>
+	<title>Insert title here</title>
+	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+	</head>
+	<body>
 	
-	<p>Connectez vous</p>
-	<form method="POST" action="../Controller/ClientController.php">
-		<table>
-		<tr>
-		<td>Login</td>
-		<td><input type="text" id="txtLogin" name="txtLogin" required="required"></td>
-		</tr>
-		<tr>
-		<td>Mot de passe</td>
-		<td><input type="text" id="txtMdp" name="txtMdp" required="required"></td>
-		</tr>
-		</table>
-		<input type="submit" value=Valider>
-	</form>
+		<?php include("header.php"); ?>
+		
+		<div id="bloc_page">
 
-	</div>
-</body>
+		<div id="banni�re">
+				<div id="banni�re_description">
+				<b>Bienvenue sur le site troc de temps! </b>
+				</div>		
+			</div>
+		
+		<p>Connectez vous</p>
+		<form method="POST" action="../Controller/ClientController.php">
+			<table>
+			<tr>
+			<td>Login</td>
+			<td><input type="text" id="txtLogin" name="txtLogin" required="required"></td>
+			</tr>
+			<tr>
+			<td>Mot de passe</td>
+			<td><input type="text" id="txtMdp" name="txtMdp" required="required"></td>
+			</tr>
+			</table>
+			<input type="submit" value=Valider>
+		</form>
+
+		</div>
+		
+		<?php include("footer.php"); ?>
+	</body>
 </html>
