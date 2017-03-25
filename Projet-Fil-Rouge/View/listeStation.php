de74141f2c1a086274940d104dc116e22d0f5e8c
Lat-ta <laetitia.carvalho@gmail.com>
2017-03-25 14:27:24 +0100

    Organisation + CSS etc

--- Projet-Fil-Rouge/View/listeStation.php
+++ Projet-Fil-Rouge/View/listeStation.php
@@ -1,29 +1,41 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>Localisation des Stations</title>
-</head>
-<body >
-<h1 id="titre">Les Stations</h1>
-<table>
-<tr>
-    <td>Identifiant</td>
-    <td>Emplacement</td>
-    <td>Capacité</td>
+	<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="style.css"/>
+	<title>Localisation des Stations</title>
+	</head>
+	
+	<body >
+	
+	<?php include("header.php"); ?>
+	
+	<h1 id="titre">Les Stations</h1>
+		<table>
+			<tr>
+				<td>Identifiant</td>
+				<td>Emplacement</td>
+				<td>Capacité</td>
 
-</tr>
-<?php 
-require_once '../Controller/StationController.php';
+			</tr>
+		<?php 
+		require_once '../Controller/StationController.php';
 
-$stations = ModelStation::getLesStations(); 
-foreach ($stations as $station):?>
-<tr>
-    <td> <?php echo $station->getIdStation()?> </td>
-    <td> <?php echo $station->getEmplacementStation() ?> </td>
-    <td><?php echo $station->getCapaciteStation()?></td>
-</tr>
-<?php endforeach;?>
-</table>
-</body>
+		$stations = ModelStation::getLesStations(); 
+		foreach ($stations as $station):?>
+		
+			<tr>
+				<td> <?php echo $station->getIdStation()?> </td>
+				<td> <?php echo $station->getEmplacementStation() ?> </td>
+				<td><?php echo $station->getCapaciteStation()?></td>
+			</tr>
+			
+		<?php endforeach;?>
+		
+		</table>
+		
+		<?php include("footer.php"); ?>
+		
+	</body>
+	
 </html>
--- Projet-Fil-Rouge/View/listeVelo.php
+++ Projet-Fil-Rouge/View/listeVelo.php
@@ -1,34 +1,53 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>Localisation des Stations</title>
-</head>
-<body >
-<h1 id="titre">Les Stations</h1>
-<table>
-<tr>
-    <td>Identifiant</td>
-    <td>Statut</td>
-    <td>Identifiant Borne</td>
-	<td>Identifiant Station</td>
-	<td>Nom Station</td>
-
+	<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="style.css"/>
+	<title>Liste des Vélos</title>
+	</head>
 	
-</tr>
-<?php 
-require_once '../Controller/VeloController.php';
+	<body >
+	<?php include("header_gestion.php"); ?>
+	
+	<h1 id="titre">Les Vélos</h1>
+		<table>
+			<tr>
+				<td>Identifiant</td>
+				<td>Statut</td>
+				<td>Identifiant Borne</td>
+				<td>Identifiant Station</td>
+				<td>Nom Station</td>
 
-$velos = ModelVelo::getLesVelos(); 
-foreach ($velos as $velo):?>
-<tr>
-    <td> <?php echo $velo->getIdVelo()?> </td>
-    <td> <?php echo $velo->getStatutVelo() ?> </td>
-    <td><?php echo $velo->getIdBorne()?></td>
-	<td><?php echo $velo->getIdStation()?></td>
-	<td><?php echo $velo->getNomStation()?></td>
-</tr>
-<?php endforeach;?>
-</table>
-</body>
+				
+			</tr>
+		<?php 
+		require_once '../Controller/VeloController.php';
+
+		$velos = ModelVelo::getLesVelos(); 
+		foreach ($velos as $velo):?>
+		
+			<tr>
+				<td> <?php echo $velo->getIdVelo()?> </td>
+				<td> <?php echo $velo->getStatutVelo() ?> </td>
+				<td><?php echo $velo->getIdBorne()?></td>
+				<td><?php echo $velo->getIdStation()?></td>
+				<td><?php echo $velo->getNomStation()?></td>
+			</tr>
+			
+		<?php endforeach;?>
+		
+		</table>
+		
+		<footer>
+			
+			<ul id="footer-menu">
+				<li>
+				<b>Intranet Vélo'v ©</b>
+				</li>
+			</ul>
+		
+		</footer>
+		
+	</body>
+	
 </html>
