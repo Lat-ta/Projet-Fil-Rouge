66c8bcf360ca1b847d88c11eb320e83bf5a77be0
Lat-ta <laetitia.carvalho@gmail.com>
2017-03-30 23:46:24 +0200

    liste vélo

--- fil-rouge-test/view/admin/listeVelo.php
+++ fil-rouge-test/view/admin/listeVelo.php
@@ -1,51 +1,37 @@
-<!DOCTYPE html>
-<html>
-	<head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="style.css"/>
-	<title>Localisation des Vélos</title>
-	</head>
+<?php $titre = 'Informations sur les vélos'; ?>
+
+<?php ob_start(); ?>
+
+	<h1 class="page-header">Liste de tous les vélos</h1>
 	
-	<body >
+	<div class="row">
 
-	<?php include("header_gestion.php"); ?>
-		<?php include("menu_gestion.php"); ?>
-	<h1 id="titre">Les Vélos</h1>
-		<table>
-		<tr>
-			<td>Identifiant</td>
-			<td>Statut</td>
-			<td>Identifiant Borne</td>
-			<td>Identifiant Station</td>
-			<td>Nom Station</td>
+		<div class="col-md-5">
 
-			
-		</tr>
-		<?php 
-		require_once '../Controller/VeloController.php';
-		
+			<table class="table table-striped">
+				<tr>
+					<td>Identifiant</td>
+					<td>Statut</td>
+					<td>Identifiant Borne</td>
+					<td>Identifiant Station</td>
+					<td>Nom Station</td>
 
-		$velos = ModelVelo::getLesVelos(); 
-		foreach ($velos as $velo):?>
-		<tr>
-			<td> <?php echo $velo->getIdVelo()?> </td>
-			<td> <?php echo $velo->getStatutVelo() ?> </td>
-			<td><?php echo $velo->getIdBorne()?></td>
-			<td><?php echo $velo->getIdStation()?></td>
-			<td><?php echo $velo->getNomStation()?></td>
-		</tr>
-		<?php endforeach;?>
-		</table>
-		
-		<footer>
-			
-			<ul id="footer-menu">
-				<li>
-				<b>Intranet Vélo'v ©</b>
-				</li>
-			</ul>
-		
-		</footer>
-		
-	</body>
-</html>
+					
+				</tr>
+				<?php 
+				foreach ($velos as $velo){?>
+				<tr>
+					<td> <?php echo $velo->getIdVelo()?> </td>
+					<td> <?php echo $velo->getStatutVelo() ?> </td>
+					<td><?php echo $velo->getIdBorne()?></td>
+					<td><?php echo $velo->getIdStation()?></td>
+					<td><?php echo $velo->getNomStation()?></td>
+				</tr>
+				<?php } ?>
+			</table>
+		</div>
+<?php $contenu = ob_get_clean(); ?>
+
+
+
+<?php require_once ("view/gabarit_admin.php"); ?>
